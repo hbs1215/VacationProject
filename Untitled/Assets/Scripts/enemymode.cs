f4e3e4402c1849a5757396f71ab3fda272cf8664
wnsdldla <twins930221@hanmail.net>
2017-01-31 00:46:48 +0900

    battle

---
+++ Untitled/Assets/Scripts/enemymode.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class enemymode : MonoBehaviour {
+
+    enum ENEMYSTATE
+    {
+        NONE = -1,
+        IDLE = 0,
+        MOVE,
+        ATTACK,
+        DAMAGE,
+        DEAD
+    }
+    bool playerattack=true;
+    ENEMYSTATE enemyState = ENEMYSTATE.IDLE;
+    public Slider healthSlider;
+    int healthPoint = 100;
+    public int enemyhealth = 100;
+    public int enemypower = 10;
+    public int playerpower = 20;
+    bool isDead=false;
+    public bool animationeffect;
+    float stateTime = 0.0f;
+    float animtime = 0.0f;
+    [Header("Idle")]
+    public float idleStateMaxTime = 2.0f;
+
+    Animator anim;
+    Animation anima;
+    Transform target = null;
+    CharacterController characterController = null;
+
+    [Header("move")]
+
+    public float moveSpeed = 5.0f;//스크롤로 바뀐다
+    public float rotationSpeed = 10.0f;
+    public float attackRange = 5f;
+
+    public float attackStateMaxTime = 2.0f;
+
+    playermode playerstate = null;
+    bool walk=false;
+
+    void Awake()
+    {
+        InitDinoSaurs();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        characterController = GetComponent<CharacterController>();
+        if(animationeffect==false)
+            anim = GetComponent<Animator>();
+        else
+            anima = GetComponent<Animation>();
+        playerstate = target.GetComponent<playermode>();
+    }
+
+ //   void Start()
+//    {
+        
+ //   }
+
+
+    void OnEnable()
+    {
+        InitDinoSaurs();
+    }
+
+    void InitDinoSaurs()
+    {
+        healthPoint = enemyhealth;
+
+        enemyState = ENEMYSTATE.IDLE;
+       
+    }
+
+    public void AciveDamage()
+    {
+        enemyState = ENEMYSTATE.DAMAGE;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.name.Contains("tiger_idle_2") == false)
+            return;
+
+        enemyState = ENEMYSTATE.DAMAGE;
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        if (Input.GetKey(KeyCode.Z))
+            playerattack = true;
+        else if (Input.GetKey(KeyCode.X))
+            playerattack = false;
+
+        if (healthPoint <= 0)
+            enemyState = ENEMYSTATE.DEAD;
+
+
+        switch (enemyState)
+        {
+            case ENEMYSTATE.IDLE:
+                {
+                    if (animationeffect == false)
+                        anim.SetBool("Move_bat", walk);
+                    else
+                        anima.CrossFade("Idle");
+                    stateTime += Time.deltaTime;
+                    if (stateTime > idleStateMaxTime)
+                    {
+                        stateTime = 0.0f;
+                        enemyState = ENEMYSTATE.MOVE;
+                        walk = true;
+                    }
+                }
+                break;
+            case ENEMYSTATE.MOVE:
+                {
+                    if (animationeffect == false)
+                        anim.SetBool("Move_bat", walk);
+                    else
+                        anima.CrossFade("Walk");
+                    float distance = (target.position - transform.position).magnitude;
+                   
+                    if (distance < attackRange)
+                    {
+                        enemyState = ENEMYSTATE.ATTACK;
+
+                        stateTime = attackStateMaxTime;
+                    }
+                    else
+                    {
+                        
+
+                        dir.y = 0.0f;
+                        dir.Normalize();
+                        characterController.SimpleMove(dir * moveSpeed);
+
+                        
+                    }
+                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), rotationSpeed * Time.deltaTime);
+
+                }
+                break;
+            case ENEMYSTATE.ATTACK:
+                {
+                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), rotationSpeed * Time.deltaTime);
+                    walk = false;
+                    if (playerattack)
+                    {
+                        stateTime += Time.deltaTime;
+                        if (stateTime > attackStateMaxTime)
+                        {
+
+                            if (animtime == 0)
+                            {
+                                if (animationeffect == false)
+                                    anim.SetTrigger("Isattack_bat");
+                                else
+                                    anima.CrossFade("Attack1");
+                            }
+                            animtime += Time.deltaTime;
+                            if (animtime > 0.4f)
+                            {
+                                playerstate.DamageByEnemy(enemypower);
+
+                                animtime = 0.0f;
+                                stateTime = 0.0f;
+                            }
+                        }
+                    }
+
+                    float distance = (target.position - transform.position).magnitude;
+                    if (distance > attackRange)
+                    {
+                        enemyState = ENEMYSTATE.IDLE;
+                        
+                    }
+                }
+                break;
+            case ENEMYSTATE.DAMAGE:
+                /*{
+
+                    healthPoint -= playerpower;
+                    healthSlider.value = healthPoint;
+                    
+
+                    AnimationState animState = anim.PlayQueued("slime_idle", QueueMode.PlayNow);
+                    animState.speed = 3.0f;
+                    anim.PlayQueued("slime_idle", QueueMode.CompleteOthers);
+
+                    stateTime = 0.0f;
+                    enemyState = ENEMYSTATE.IDLE;
+
+                    if (healthPoint <= 0)
+                        enemyState = ENEMYSTATE.DEAD;
+                }*/
+                break;
+            case ENEMYSTATE.DEAD:
+                {
+                    //Destroy(gameObject);
+                    if (animationeffect == false)
+                        anim.SetTrigger("Isdead_bat");
+                    else
+                        anima.CrossFade("Death");
+                    StartCoroutine("DeadProcess");
+                    
+                    enemyState = ENEMYSTATE.NONE;
+
+
+                }
+                break;
+        }
+
+    }
+    
+    public void DamageByEnemy(int power)
+    {
+        if (isDead)
+            return;
+
+        healthPoint = healthPoint - power;
+        healthSlider.value = healthPoint;
+        Debug.Log(healthPoint);
+
+        //cameraShake.PlayCameraShake();
+
+        if (healthPoint <= 0)
+        {
+            isDead = true;
+        }
+    }
+
+    IEnumerator DeadProcess()
+    {
+        
+       
+            
+
+        
+
+        yield return new WaitForSeconds(1.0f);
+        
+        // Destroy(gameObject);
+        gameObject.SetActive(false);
+    }
+    IEnumerator attackProcess()
+    {
+        
+            yield return new WaitForSeconds(2.0f);
+        
+    }
+}
---
+++ Untitled/Assets/Scripts/playermode.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playermode : MonoBehaviour {
+
+    enum ENEMYSTATE
+    {
+        NONE = -1,
+        IDLE = 0,
+        MOVE,
+        ATTACK,
+        KILL,
+        DEAD,
+        ESCAPE
+    }
+  
+    ENEMYSTATE enemyState = ENEMYSTATE.IDLE;
+    public Slider healthSlider;
+    int healthPoint = 2;
+    public int playerhealth = 100;
+    public int enemypower = 10;
+    public int playerpower = 20;
+    bool isDead=false;
+    bool walk=false;
+    float stateTime = 0.0f;
+    float animationtime = 0.0f;
+    [Header("Idle")]
+    public float idleStateMaxTime = 2.0f;
+
+    Animator anim;
+    Transform target = null;
+    Transform character = null;
+    CharacterController characterController = null;
+
+    [Header("move")]
+
+    public float moveSpeed = 5.0f;//스크롤로 바뀐다
+    public float rotationSpeed = 10.0f;
+    public float attackRange = 5f;
+
+    public float attackStateMaxTime = 3.0f;
+
+    enemymode enemystate = null;
+
+    Vector3 escape;
+    void Awake()
+    {
+        InitDinoSaurs();
+        target = GameObject.FindGameObjectWithTag("Enemy").transform;
+        character = GameObject.FindGameObjectWithTag("character").transform;
+        
+        characterController = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        enemystate = target.GetComponent<enemymode>();
+    }
+
+    //   void Start()
+    //    {
+
+    //   }
+
+
+
+    void InitDinoSaurs()
+    {
+        healthPoint = playerhealth;
+
+        enemyState = ENEMYSTATE.IDLE;
+        //anim["slime_idle"].speed = 1.5f;
+        //anim.CrossFade("slime_idle");
+    }
+
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Z))
+            enemyState = ENEMYSTATE.MOVE;
+        else if (Input.GetKey(KeyCode.X))
+            enemyState = ENEMYSTATE.ESCAPE;
+
+        Vector3 dir = target.position - transform.position;
+        if (GameObject.FindGameObjectWithTag("Enemy")!= null)
+            target = GameObject.FindGameObjectWithTag("Enemy").transform;
+        if(target!=null)
+            enemystate = target.GetComponent<enemymode>();
+
+        anim.SetBool("walking", walk);
+        if (healthPoint <= 0)
+            enemyState = ENEMYSTATE.DEAD;
+        switch (enemyState)
+        {
+            case ENEMYSTATE.IDLE:
+                {
+                    stateTime += Time.deltaTime;
+                    walk = false;
+                    if (stateTime > idleStateMaxTime)
+                    {
+                        stateTime = 0.0f;
+                        enemyState = ENEMYSTATE.MOVE;
+  
+                    }
+                }
+                break;
+            case ENEMYSTATE.MOVE:
+                {
+                    walk = true;
+                    float distance = (target.position - transform.position).magnitude;
+                    Debug.Log(distance);
+                    if (distance < attackRange)
+                    {
+                        enemyState = ENEMYSTATE.ATTACK;
+                    }
+                    else
+                    {
+
+                        dir = target.position - transform.position;
+
+                        //Debug.Log(transform.position);
+                        // Debug.Log(dir);
+
+                        dir.y = 0.0f;
+                        dir.Normalize();
+                        characterController.SimpleMove(dir * moveSpeed);
+
+                        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), rotationSpeed * Time.deltaTime);
+
+                    }
+                }
+                break;
+            case ENEMYSTATE.ATTACK:
+                {
+                    stateTime += Time.deltaTime;
+                    dir = target.position - transform.position;
+                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), rotationSpeed * Time.deltaTime);
+
+                    if (stateTime > attackStateMaxTime)
+                    {
+                        if(animationtime == 0)
+                            anim.SetTrigger("Isattack_tiger");
+                        animationtime += Time.deltaTime;
+                        if (animationtime>0.6f)
+                        {
+                           
+                            enemystate.DamageByEnemy(playerpower);
+                            animationtime = 0f;
+                            stateTime = 0.0f;
+                        }
+                           
+                        walk = false;
+                    }
+                   
+                    float distance = (target.position - transform.position).magnitude;
+                    if (distance > attackRange)
+                    {
+                        enemyState = ENEMYSTATE.IDLE;
+                    }
+                    
+                }
+                break;
+            case ENEMYSTATE.KILL:
+               
+                break;
+            case ENEMYSTATE.DEAD:
+                {
+                    //Destroy(gameObject);
+                    anim.SetTrigger("Die");
+                    StartCoroutine("DeadProcess");
+                    enemyState = ENEMYSTATE.NONE;
+                    
+                }
+                break;
+            case ENEMYSTATE.ESCAPE:
+                {
+                    walk = true;
+                    escape = character.position;
+                    escape.z -= 2;
+
+                     dir = escape - transform.position;
+                    if (dir.magnitude > 2f)
+                    {
+
+
+                        dir.y = 0.0f;
+                        dir.Normalize();
+                        characterController.SimpleMove(dir * moveSpeed);
+
+                        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), rotationSpeed * Time.deltaTime);
+
+                    }
+                        
+                    
+
+                }
+                break;
+
+        }
+
+    }
+
+
+
+    public void DamageByEnemy(int number)
+    {
+        if (isDead)
+            return;
+
+        healthPoint = healthPoint - number;
+        healthSlider.value = healthPoint;
+        //cameraShake.PlayCameraShake();
+
+        if (healthPoint <= 0)
+        {
+            isDead = true;
+        }
+    }
+
+    IEnumerator DeadProcess()
+    {
+        //anim["slime_die"].speed = 2.0f;
+        //anim.Play("slime_die");
+        
+        yield return new WaitForSeconds(2.0f);
+        
+        // Destroy(gameObject);
+        gameObject.SetActive(false);
+    }
+    IEnumerator attackProcess()
+    {
+        //anim["slime_attack"].speed = 2.0f;
+        //anim.Play("slime_attack");
+
+       
+            yield return new WaitForEndOfFrame();
+        
+    }
+}
